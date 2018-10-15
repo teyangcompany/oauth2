a7fb744fab0dfb78a59f58b2fcc0ed2f5f0e44db
lmw6412036 <lmw6412036@sina.com>
2018-10-15 09:47:16 +0800

    增加日志监控

--- src/main.js
+++ src/main.js
@@ -3,18 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import VueCookies from "vue-cookies"
+import 'babel-polyfill'
 
 
 import * as Sentry from '@sentry/browser'
 
-Sentry.init({
+process.env.NODE_ENV === 'production' && Sentry.init({
   dsn: 'https://4e1d487fd4744afd932c4347edb3cfd6@sentry.io/1299841',
-  integrations: [new Sentry.Integrations.Vue({ Vue })]
+  integrations: [new Sentry.Integrations.Vue({Vue})]
 })
 
 
 Vue.use(VueCookies);
-
 
 Vue.config.productionTip = false
 
